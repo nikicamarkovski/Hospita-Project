95e5563c72ff19f3dfc6bd5ece810bb9f28de933
Nikica <markovskinikica@gmail.com>
2019-12-11 14:54:50 +0100

    symptoms and illness routes

--- symptoms_illness/routes.js
+++ symptoms_illness/routes.js
@@ -4,5 +4,4 @@ const routes = express.Router();
 const middleware = require('../middleware/common');
 
 routes.get('/patients/diagnose/:desc' ,[middleware.checkToken,middleware.verifyToken], actions.Diagnose);
-
 module.exports = routes;
