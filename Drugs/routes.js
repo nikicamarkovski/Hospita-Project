f5ecac15c01a480cc93f183a0cc9095837c1cc8f
Nikica <markovskinikica@gmail.com>
2019-12-11 14:36:22 +0100

    routes for drugs

--- Drugs/routes.js
+++ Drugs/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const actions = require('./actions')
 const routes = express.Router();
+
 const middleware = require('../middleware/common');
 
 routes.get('/drugs/:name' , [middleware.checkToken,middleware.verifyToken],actions.NumberOfMedications);
